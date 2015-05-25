4fe83306f56f5a6948badfad9e86dce558540651
Julien Guittard <julien.guittard@me.com>
2015-05-25 13:05:45 +0200

    Added SQL structure and data

---
+++ data/sample/phonebooklib-inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Nicolas", "Debras", "nicolas.debras@athome.eu", "+352 621 111 222", "Joli brushing", "2015-05-10 02:03:04");
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Anthony", "Laurain", "anthony.laurain@reagroup.com", "+352 691 333 444", "Bisounours", "2015-05-11 17:00:23");
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Christophe", "Di Prima", "christophe.diprima@reagroup.com", NULL, "Dwarf power", "2015-05-11 15:43:34");
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Christian", "Schramm Do Carmo", "chritian.schramm-do-carmo@reagroup.com", NULL, NULL, "2015-05-14 08:03:44");
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Joachim", "Justh", "joachim.justh@reagroup.com", "+352 621 555 666", "Il na des blagues pourries", "2015-05-15 07:09:55");
+INSERT INTO contact (title, firstname, lastname, email, phone, notes, createdAt) VALUES ("mr", "Jérôme", "De Almeida", "jerome.dealmeida@vesperiagroup.com", NULL, "", "2015-05-18 13:45:16");
---
+++ data/sample/phonebooklib-tables.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE "contact" (
+    id INTEGER NOT NULL PRIMARY KEY,
+    title VARCHAR(4) NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NULL,
+    address VARCHAR(2000) NULL,
+    phone VARCHAR(20) NULL,
+    notes VARCHAR(2000) NULL,
+    avatar VARCHAR(255) NULL,
+    createdAt VARCHAR(20) NOT NULL,
+    updatedAt VARCHAR(20) NULL
+);
+COMMIT;
